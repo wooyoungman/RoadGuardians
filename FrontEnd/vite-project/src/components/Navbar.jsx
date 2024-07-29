65bee525a26344cee056f73cfa2c7de7e4a079e9
박민철 <shoostar0611@gmail.com>
2024-07-29 15:06:09 +0900

    feat : [박민철] Kakao Map API 추가

--- FrontEnd/vite-project/src/components/Navbar.jsx
+++ FrontEnd/vite-project/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         </NavLink>
       </div>
       <ul className="nav-links">
-        <li><NavLink to="/" exact activeClassName="active">포트홀 지도</NavLink></li>
+        <li><NavLink to="/" activeClassName="active">포트홀 지도</NavLink></li>
         <li><NavLink to="/stats" activeClassName="active">통계</NavLink></li>
         <li><NavLink to="/link" activeClassName="active">연계 내역</NavLink></li>
         <li><NavLink to="/report" activeClassName="active">신고 내역</NavLink></li>
