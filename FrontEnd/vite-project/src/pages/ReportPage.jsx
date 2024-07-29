203801ee751def2cea76d1963fa881b6f435ae0c
지민경 <jming31@naver.com>
2024-07-29 14:06:04 +0900

    feat : [지민경] 스켈레톤 코드 생성

---
+++ FrontEnd/vite-project/src/pages/ReportPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReportPage = () => {
+  return (
+    <div>
+      <h1>신고 접수 페이지</h1>
+      <p>여기에 신고 접수 페이지 내용이 들어갑니다.</p>
+    </div>
+  );
+};
+
+export default ReportPage;
