fbac1fb48cd644432a99a32952fc7fdadfa4062f
Chung <souffle1903@gmail.com>
2024-08-02 10:13:38 +0900

    feat : [정하림] firebase 연결

---
+++ backend/src/main/java/com/c104/guardians/config/FirebaseConfig.java
@@ -0,0 +1,33 @@
+package com.c104.guardians.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp initializeFirebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("c104-10f5a.appspot.com")
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public StorageClient storageClient(FirebaseApp firebaseApp) {
+        return StorageClient.getInstance(firebaseApp);
+    }
+
+}
