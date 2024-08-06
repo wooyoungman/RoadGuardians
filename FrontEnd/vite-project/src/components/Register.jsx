b60d6f6ae5a0eb1a03d7178a380e88c3c5d86c9c
Chung <souffle1903@gmail.com>
2024-08-07 00:29:55 +0900

    feat : [정하림] 회원가입 - 부서 선택 추가
    Former-commit-id: 053bf84f481d2ca674a8f75ef45abb6810dc5723

--- FrontEnd/vite-project/src/components/Register.jsx
+++ FrontEnd/vite-project/src/components/Register.jsx
@@ -11,7 +11,8 @@ function Register() {
     name: '',
     email: '',
     phone: '',
-    user_type: '운영관리팀', // 초기 선택값 설정
+    // user_type: '운영관리', // 초기 선택값 설정
+    dept_name: '소속 부서'
   });
   const [isIdValid, setIsIdValid] = useState(null); // ID 유효성 상태
   const navigate = useNavigate();
@@ -26,8 +27,11 @@ function Register() {
     }
   };
 
-  const handleTeamChange = (e) => {
-    setFormData({ ...formData, user_type: e.target.value });
+  // const handleTeamChange = (e) => {
+  //   setFormData({ ...formData, user_type: e.target.value });
+  // };
+  const handleDeptChange = (e) => {
+    setFormData({ ...formData, dept_name: e.target.value });
   };
 
   const handleIdCheck = async () => {
@@ -67,7 +71,8 @@ function Register() {
     }
 
     // 팀 값 변환
-    const userTypeValue = formData.user_type === '운영관리팀' ? 1 : 2;
+    // const userTypeValue = formData.user_type === '운영관리' ? 1 : 2;
+    // 부서
 
     try {
       const response = await axios.post('https://i11c104.p.ssafy.io/api/v1/auth/signUp', {
@@ -77,8 +82,19 @@ function Register() {
         confirmPassword: formData.passwordConfirm,
         name: formData.name,
         phoneNumber: formData.phone,
-        userType: userTypeValue,
+        userType: formData.dept_name === "1" ? 1 : 2,
+        deptId: parseInt(formData.dept_name),
       });
+      console.log({
+        id: formData.id,
+        email: formData.email,
+        password: formData.password,
+        confirmPassword: formData.passwordConfirm,
+        name: formData.name,
+        phoneNumber: formData.phone,
+        userType: formData.dept_name > "1" ? 1 : 2,
+        deptId: parseInt(formData.dept_name),
+      })
 
       if (response.data.result) {
         alert('회원가입이 완료되었습니다.');
@@ -87,8 +103,23 @@ function Register() {
         alert(`회원가입 실패: ${response.data.message}`);
       }
     } catch (error) {
+      if (isNaN(formData.dept_name)){
+        alert('소속 부서를 선택해주세요.');
+      } else {
       console.error('Registration error', error);
       alert('회원가입 중 오류가 발생했습니다.');
+      }
+      console.log({
+        id: formData.id,
+        email: formData.email,
+        password: formData.password,
+        confirmPassword: formData.passwordConfirm,
+        name: formData.name,
+        phoneNumber: formData.phone,
+        userType: formData.dept_name > "1" ? 1 : 2,
+        deptId: parseInt(formData.dept_name),
+      })
+      
     }
   };
 
@@ -117,10 +148,22 @@ function Register() {
         <label htmlFor="phone">전화번호 :</label>
         <input type="text" id="phone" name="phone" value={formData.phone} onChange={handleChange} />
 
-        <label htmlFor="team">팀 선택 :</label>
+        {/* <label htmlFor="team">운영관리 / 유지보수 :</label>
         <select id="team" name="user_type" value={formData.user_type} onChange={handleTeamChange}>
-          <option value="운영관리팀">운영관리팀</option>
-          <option value="유지보수팀">유지보수팀</option>
+          <option value="운영관리">운영관리</option>
+          <option value="유지보수">유지보수</option>
+        </select> */}
+
+        <label htmlFor="team">소속 부서를 선택해주세요. :</label>
+        <select id="dept_name" name="dept_name" value={formData.dept_name} onChange={handleDeptChange}>
+          <option value="-1">소속 부서</option>
+          <option value="1">운영관리팀</option>
+          <option value="2">광산구 유지보수팀</option>
+          <option value="3">동구 유지보수팀</option>
+          <option value="4">서구 유지보수팀</option>
+          <option value="5">남구 유지보수팀</option>
+          <option value="6">북구 유지보수팀</option>
+
         </select>
 
         <button type="submit">회원가입</button>
